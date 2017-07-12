5537a338fabed5d383634a77bd566da4cc9c8f72
konstantins304 <konstantins304@gmail.com>
2017-07-12 18:26:04 +0400

    -- add advice for controller

--- src/main/java/org/mycompany/Application.java
+++ src/main/java/org/mycompany/Application.java
@@ -2,8 +2,13 @@ package org.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "org.mycompany")
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@ComponentScan("org.mycompany")
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/org/mycompany/controllers/GeneralAdviceController.java
+++ src/main/java/org/mycompany/controllers/GeneralAdviceController.java
@@ -1,7 +1,37 @@
 package org.mycompany.controllers;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by konstantinamp on 7/9/17.
  */
+@ControllerAdvice(annotations = RestController.class)
 public class GeneralAdviceController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GeneralAdviceController.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public Map<String, Object> generalError(Exception ex) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", ex.getMessage() != null ? ex.getMessage() : "Unknown error");
+        map.put("type", ex.getClass().getSimpleName());
+        map.put("stack", ExceptionUtils.getStackTrace(ex));
+
+        LOG.error(ex.getMessage(), ex);
+
+        return map;
+    }
 }
