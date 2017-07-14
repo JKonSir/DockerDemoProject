21c17893d58c1055362e61a01660687be27c5514
konstantins304 <konstantins304@gmail.com>
2017-07-14 21:23:50 +0400

    -- add swagger settings bean -- add api info -- change port on 8080

--- src/main/java/org/mycompany/Application.java
+++ src/main/java/org/mycompany/Application.java
@@ -2,8 +2,16 @@ package org.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.ArrayList;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -14,4 +22,24 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public Docket swaggerSettings() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("org.mycompany.controllers"))
+                .build()
+                .apiInfo(apiMetaData());
+    }
+
+    private ApiInfo apiMetaData() {
+        return new ApiInfo("DockerDemoProject REST API",
+                "api for manage subjects",
+                "1.0 - for postgres, 2.0 - for mongo",
+                "Terms of service",
+                new Contact("konstantins304", "https://github.com/konstantins304/DockerDemoProject", "konstantins304@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+
 }
